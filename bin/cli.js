a599becf118dda67d253b82a45011dc4463c95da
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-05-09 17:47:08 +0200

    Add CLI for session tracking

--- bin/cli.js
+++ bin/cli.js
@@ -1,1 +1,18 @@
 #!/usr/bin/env node
+import { Command } from "commander";
+import { startTracking, stopTracking } from "../lib/index.js";
+
+const program = new Command();
+
+program.name("tdt").description("Track Dev Time CLI").version("0.0.1");
+
+program
+  .command("start")
+  .description("Start coding session")
+  .action(startTracking);
+program
+  .command("stop")
+  .description("Stop and save session")
+  .action(stopTracking);
+
+program.parse(process.argv);
