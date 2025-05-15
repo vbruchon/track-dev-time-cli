5de5f3bbe62b61f02087804b6ca1bfe1321f64ef
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-05-15 16:35:09 +0200

    implement setup command to update dev script and gitignore

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 import { Command } from "commander";
 import { startTracking, stopTracking } from "../lib/index.js";
+import { setupPackage } from "../lib/setup.js";
 
 const program = new Command();
 
-program.name("tdt").description("Track Dev Time CLI").version("0.0.1");
+program
+  .name("track-dev-time")
+  .description("Track Dev Time CLI")
+  .version("0.0.1");
 
 program
   .command("start")
@@ -14,5 +18,6 @@ program
   .command("stop")
   .description("Stop and save session")
   .action(stopTracking);
+program.command("setup ").description("Setup package").action(setupPackage);
 
 program.parse(process.argv);
