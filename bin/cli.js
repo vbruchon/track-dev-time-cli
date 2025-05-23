18d48096a2a0624055a92531ba924265bf86212d
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2025-05-23 18:49:09 +0200

    implement list sessions

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { Command } from "commander";
-import { startTracking, stopTracking } from "../lib/index.js";
+import { listTracking, startTracking, stopTracking } from "../lib/index.js";
 import { setupPackage } from "../lib/setup.js";
 
 const program = new Command();
@@ -19,5 +19,8 @@ program
   .description("Stop and save session")
   .action(stopTracking);
 program.command("setup").description("Setup package").action(setupPackage);
+program.command("list").description("List all sessions").action(listTracking);
 
 program.parse(process.argv);
+
+// Ajouter une commande list (liste toutes les sessions)
